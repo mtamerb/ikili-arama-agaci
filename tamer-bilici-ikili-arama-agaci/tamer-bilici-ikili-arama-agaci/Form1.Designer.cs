08dc56c3e53ae076083033a100fb6f6110579bb2
Tamer Bilici <64536219+mtamerb@users.noreply.github.com>
2023-01-11 21:39:40 +0300

    Add files via upload
    add: all files

---
+++ tamer-bilici-ikili-arama-agaci/tamer-bilici-ikili-arama-agaci/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace tamer_bilici_ikili_arama_agaci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.txtbox_ekle = new System.Windows.Forms.TextBox();
+            this.txtbox_sil = new System.Windows.Forms.TextBox();
+            this.txtbox_bul = new System.Windows.Forms.TextBox();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.btn_bul = new System.Windows.Forms.Button();
+            this.btn_show = new System.Windows.Forms.Button();
+            this.txtbox_show = new System.Windows.Forms.TextBox();
+            this.btn_bilgi_goster = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(37, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "-düğüm eklendi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(37, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "-düğüm silindi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Location = new System.Drawing.Point(37, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Düğümün Düzeyi";
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.Location = new System.Drawing.Point(248, 43);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(99, 23);
+            this.btn_ekle.TabIndex = 4;
+            this.btn_ekle.Text = "Düğüm Ekle";
+            this.btn_ekle.UseVisualStyleBackColor = true;
+            this.btn_ekle.Click += new System.EventHandler(this.btn_ekle_Click);
+            // 
+            // txtbox_ekle
+            // 
+            this.txtbox_ekle.Location = new System.Drawing.Point(131, 48);
+            this.txtbox_ekle.Name = "txtbox_ekle";
+            this.txtbox_ekle.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_ekle.TabIndex = 5;
+            // 
+            // txtbox_sil
+            // 
+            this.txtbox_sil.Location = new System.Drawing.Point(131, 86);
+            this.txtbox_sil.Name = "txtbox_sil";
+            this.txtbox_sil.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_sil.TabIndex = 6;
+            // 
+            // txtbox_bul
+            // 
+            this.txtbox_bul.Location = new System.Drawing.Point(131, 130);
+            this.txtbox_bul.Name = "txtbox_bul";
+            this.txtbox_bul.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_bul.TabIndex = 7;
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Location = new System.Drawing.Point(248, 83);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(99, 23);
+            this.btn_sil.TabIndex = 8;
+            this.btn_sil.Text = "Düğüm Silme";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            this.btn_sil.Click += new System.EventHandler(this.btn_sil_Click);
+            // 
+            // btn_bul
+            // 
+            this.btn_bul.Location = new System.Drawing.Point(248, 128);
+            this.btn_bul.Name = "btn_bul";
+            this.btn_bul.Size = new System.Drawing.Size(99, 23);
+            this.btn_bul.TabIndex = 9;
+            this.btn_bul.Text = "Düğüm Bul";
+            this.btn_bul.UseVisualStyleBackColor = true;
+            this.btn_bul.Click += new System.EventHandler(this.btn_bul_Click);
+            // 
+            // btn_show
+            // 
+            this.btn_show.Location = new System.Drawing.Point(51, 197);
+            this.btn_show.Name = "btn_show";
+            this.btn_show.Size = new System.Drawing.Size(260, 23);
+            this.btn_show.TabIndex = 10;
+            this.btn_show.Text = "AĞAÇTAKİ DÜĞÜMLERİ GÖSTER";
+            this.btn_show.UseVisualStyleBackColor = true;
+            this.btn_show.Click += new System.EventHandler(this.btn_show_Click);
+            // 
+            // txtbox_show
+            // 
+            this.txtbox_show.Location = new System.Drawing.Point(51, 240);
+            this.txtbox_show.Multiline = true;
+            this.txtbox_show.Name = "txtbox_show";
+            this.txtbox_show.Size = new System.Drawing.Size(260, 136);
+            this.txtbox_show.TabIndex = 11;
+            // 
+            // btn_bilgi_goster
+            // 
+            this.btn_bilgi_goster.Location = new System.Drawing.Point(413, 38);
+            this.btn_bilgi_goster.Name = "btn_bilgi_goster";
+            this.btn_bilgi_goster.Size = new System.Drawing.Size(249, 23);
+            this.btn_bilgi_goster.TabIndex = 12;
+            this.btn_bilgi_goster.Text = "AĞAÇTAKİ BİLGİLERİNİ GÖSTER";
+            this.btn_bilgi_goster.UseVisualStyleBackColor = true;
+            this.btn_bilgi_goster.Click += new System.EventHandler(this.btn_bilgi_goster_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.textBox10);
+            this.groupBox1.Controls.Add(this.textBox9);
+            this.groupBox1.Controls.Add(this.textBox8);
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(413, 86);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(249, 290);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ağaç Bilgileri";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label10.Location = new System.Drawing.Point(6, 213);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(52, 13);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "Yapraklar";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label9.Location = new System.Drawing.Point(6, 177);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 13);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Ağacın Yüksekliği";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label8.Location = new System.Drawing.Point(6, 137);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(154, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Ağaçtaki Toplam Düğüm Sayısı";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(72, 206);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(145, 20);
+            this.textBox10.TabIndex = 21;
+            this.textBox10.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(117, 170);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 20;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(166, 134);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(51, 20);
+            this.textBox8.TabIndex = 19;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(72, 98);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(145, 20);
+            this.textBox7.TabIndex = 18;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(72, 61);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(145, 20);
+            this.textBox6.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(72, 26);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(145, 20);
+            this.textBox5.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label7.Location = new System.Drawing.Point(6, 98);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Postorder";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.Location = new System.Drawing.Point(6, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Inorder";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Location = new System.Drawing.Point(6, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Preorder";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(708, 429);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_bilgi_goster);
+            this.Controls.Add(this.txtbox_show);
+            this.Controls.Add(this.btn_show);
+            this.Controls.Add(this.btn_bul);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.txtbox_bul);
+            this.Controls.Add(this.txtbox_sil);
+            this.Controls.Add(this.txtbox_ekle);
+            this.Controls.Add(this.btn_ekle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tamer Bilici - ikili arama ağacı ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.TextBox txtbox_ekle;
+        private System.Windows.Forms.TextBox txtbox_sil;
+        private System.Windows.Forms.TextBox txtbox_bul;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.Button btn_bul;
+        private System.Windows.Forms.Button btn_show;
+        private System.Windows.Forms.TextBox txtbox_show;
+        private System.Windows.Forms.Button btn_bilgi_goster;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ tamer-bilici-ikili-arama-agaci/tamer-bilici-ikili-arama-agaci/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tamer_bilici_ikili_arama_agaci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // agaca ekleme  silme  listeleme  opr.
+
+        public BST root;
+
+        public class BST
+        {
+            public int data;
+            public BST right;
+            public BST left;
+        }
+
+        void insert(int key)
+        {
+            BST newNode = new BST();
+            newNode.data = key;
+
+            if (root == null)
+            {
+                root = newNode;
+                label2.Text = Convert.ToString(key + " eklendi");
+            }
+            else
+            {
+                BST temp = root;
+                BST top;
+                while (true)
+                {
+                    top = temp;
+                    if (key < temp.data)
+                    {
+                        temp = temp.left;
+                        if (temp == null)
+                        {
+                            top.left = newNode;
+                            label2.Text = Convert.ToString(key + " eklendi");
+                            return;
+                        }
+
+                    }
+                    else
+                    {
+                        temp = temp.right;
+                        if (temp == null)
+                        {
+                            top.right = newNode;
+                            label2.Text = Convert.ToString(key + " eklendi");
+                            return;
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+         void showNodes(BST node)
+         {
+
+             if (root != null)
+             
+                 txtbox_show.Text = "root ->  " + Convert.ToString(node.data) + Environment.NewLine;
+             
+
+             if (node.left != null)
+             {
+                 txtbox_show.Text += "left -> " + Convert.ToString(node.left.data) + Environment.NewLine;
+                 showLow(node.left, "left -> ");
+             }
+
+             if (node.right != null)
+             {
+                 txtbox_show.Text += "right -> " + Convert.ToString(node.right.data) + Environment.NewLine;
+                 showLow(node.right, "right -> ");
+             }
+
+         }
+
+         void showLow(BST node, string pointer) {
+
+             if (node.left != null)
+             {
+                 txtbox_show.Text += pointer + "left -> " + Convert.ToString(node.left.data) + Environment.NewLine;
+                 pointer += "left -> ";
+                 showLow(node.left, pointer);
+             }
+             if (node.right != null)
+             {
+                 txtbox_show.Text += pointer + "rigth -> " + Convert.ToString(node.right.data) + Environment.NewLine;
+                 pointer += "right -> ";
+                 showLow(node.right, pointer);
+             }
+
+         }
+
+
+         public BST deleteNode(BST root, int key)
+         {
+             if (root == null)
+             {
+                 return null;
+             }
+
+             if (key < root.data)
+             {
+                 root.left = deleteNode(root.left, key); 
+             }
+             if (key > root.data)
+             {
+                 root.right = deleteNode(root.right, key);
+             }
+
+             if (root.left != null && root.right != null)
+             {
+                 root.data = Max(root.left).data;
+                 root.left = deleteNode(root.left, root.data);
+             }
+             if (root.left != null)
+             {
+                 root = root.left;
+             }
+             else
+             {
+                 root = root.right;
+             }
+             return root;
+         }
+
+         public BST Max(BST root)
+         {
+             if (root == null)
+             {
+                 return null;
+             }
+             BST temp = root;
+             while (temp.right != null)
+             {
+                 temp = temp.right;
+             }
+
+             return temp;
+
+         }
+
+         public BST find(BST root, int key)
+         {
+
+             if (root == null)
+             {
+                 MessageBox.Show("can not found");
+                 return null;
+             }
+
+             else
+             {
+                 if (key == root.data)
+                 {
+                     MessageBox.Show("its found");
+                 }
+
+                 if (key < root.data)
+                 {
+                     return find(root.left, key);
+                 }
+                 if (key > root.data)
+                 {
+                     return find(root.right, key);
+                 }
+
+             }
+
+             return root;
+
+         }
+
+
+        // bilgiler 
+
+         public int numberOfNode(BST node)
+         {
+             int size = 0;
+             if (node != null)
+             {
+                 size = 1;
+                 size += numberOfNode(node.right);
+                 size += numberOfNode(node.left);
+             }
+             return size;
+         }
+
+         public int height(BST root)
+         {
+             if (root == null)
+             {
+                 return 0;
+             }
+
+             return Math.Max(height(root.left), height(root.right)) + 1;
+         }
+
+
+         public int leaf(BST node)
+         {
+             if (node == null)
+             {
+                 return 0;
+;
+             }
+             if (node.left == null && node.right == null)
+             {
+                 return 1;
+             }
+             return leaf(node.left) + leaf(node.right);
+         }
+
+
+        // ın or post _ order
+
+
+        
+         void postorder(BST root)
+         {
+             if (root == null)
+             {
+                 return;
+             }
+             postorder(root.left);
+             postorder(root.right);
+             textBox7.Text += root.data + " ";
+
+         }
+
+         void preorder(BST root)
+         {
+             if (root == null)
+             {
+                 return;
+             }
+
+             textBox5.Text += root.data + " "; 
+             preorder(root.left);
+             preorder(root.right);
+          
+         }
+         void inorder(BST root)
+         {
+             if (root == null)
+             {
+                 return;
+             }
+
+             inorder(root.left);
+             textBox6.Text += root.data + " ";
+             inorder(root.right);
+             
+
+         }
+
+
+
+
+
+
+        private void btn_ekle_Click(object sender, EventArgs e)
+        {
+            int key = Convert.ToInt32(txtbox_ekle.Text);
+            insert(key);
+        }
+
+        private void btn_show_Click(object sender, EventArgs e)
+        {
+            txtbox_show.Text = " ";
+            showNodes(root);
+        }
+
+        private void btn_sil_Click(object sender, EventArgs e)
+        {
+            int data = Convert.ToInt32(txtbox_sil.Text);
+            deleteNode(root, data);
+            label3.Text = Convert.ToString(data + " silindi ");
+        }
+
+        private void btn_bul_Click(object sender, EventArgs e)
+        {
+            int data = Convert.ToInt32(txtbox_bul.Text);
+            find(root, data);
+        }
+
+        private void btn_bilgi_goster_Click(object sender, EventArgs e)
+        {
+
+
+            preorder(root);
+            inorder(root);
+            postorder(root);
+
+
+            textBox8.Text = Convert.ToString(numberOfNode(root));
+            textBox9.Text = Convert.ToString(height(root) - 1);
+            textBox10.Text = Convert.ToString(leaf(root));
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
